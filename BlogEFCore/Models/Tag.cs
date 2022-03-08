cd31f29de0100aab3b5eef2d05ade171b94bae6c
Joel Landgraf Filho <joel.landgraf@gmail.com>
2022-03-08 05:55:18 -0300

    Modulo 1 completo

--- BlogEFCore/Models/Tag.cs
+++ BlogEFCore/Models/Tag.cs
@@ -1,7 +1,9 @@
 namespace BlogEFCore.Models;
 
+[Table("Tag")]
 public class Tag
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
     public string Slug { get; set; }
